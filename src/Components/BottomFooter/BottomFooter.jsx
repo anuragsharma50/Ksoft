3cb4320eacb4e5d985a6a1e75f99ffb9ffb42d3a
anuragsharma50 <anuragsh868@gmail.com>
2020-08-26 16:22:59 +0530

    footer added

---
+++ src/Components/BottomFooter/BottomFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './bottom-footer.styles.css'
+
+function BottomFooter() {
+    return (
+        <div className='bottom-footer'>
+            KSOFTINDIA © All Rights Reserved.2020 
+        </div>
+    )
+}
+
+export default BottomFooter
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
-import './footer.style.css'
+import './footer.style.css';
+import Logo from '../../images/logo.jpg';
+import Phone from '../../icons/phone-white.png';
+import Mail from '../../icons/mail-white.png';
+import Address from '../../icons/address-white.png';
+import FB from '../../icons/fb-white.png';
+import Twitter from '../../icons/twitter-white.png';
+import Linkedin from '../../icons/linkedin-white.png';
+
+import BottomFooter from '../BottomFooter/BottomFooter';
+
 
 function Footer() {
     return (
+        <div>
         <div className='footer'>
-            
+            <div className='footer-about'>
+                <h3>About us</h3>
+                <img className='footer-image' src={Logo} alt="Logo"/>
+                <p>KSOFT has specialization in 3 important domains namely: DEVELOPMENT, CONSULTANCY and TRAINING,. 
+                The company provides specialized training in 100+ leading technologies like Python, , Data Science,
+                Cloud, AWS, Robotics, LINUX and many more.</p>
+
+                <div className='footer-about-icons'>
+                    <img className='footer-fb' src={FB} alt="facebook"/>
+                    <img className='footer-twitter' src={Twitter} alt="twitter"/>
+                    <img className='footer-linkedin' src={Linkedin} alt="linkedin"/>
+                </div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <div className='footer-contact'>
+                <h3>Contact us</h3>
+                <div className='footer-contact-details'>
+                    <div className='footer-contact-icons'>
+                    <img className='footer-address' src={Address} alt="address"/>
+                    <img className='footer-phone' src={Phone} alt="phone"/>
+                    <img className='footer-mail' src={Mail} alt="email"/>
+                    </div>
+
+                    <div className='footer-contact-data'>
+                    <p>Head Office : D - Block, Sector -2, Noida Registered Office : C - Block, ZETA-1, Greater Noida</p>
+                    <p>+91 9990 82 6002</p>
+                    <p>info@ksoftindia.com</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+    <BottomFooter/>
+    </div>
     )
 }
 
