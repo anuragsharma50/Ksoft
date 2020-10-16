46812bd7410e7342275c466eca3ac6f7b8e78f53
anuragsharma50 <anuragsh868@gmail.com>
2020-10-16 14:19:00 +0530

    whatsapp linking working

--- src/HomeComp/Whatsapp/Whatsapp.jsx
+++ src/HomeComp/Whatsapp/Whatsapp.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import WhatsappIcon from '../../icons/whatsapp.png';
 import './whatsapp.style.css';
+import { useMediaQuery } from 'react-responsive'
+ 
+
 
 const openWhatsapp = () => {
+    window.location.assign('https://web.whatsapp.com/send?phone=919990826002');
+}
+
+const openWhatsappMobile = () => {
     window.location.assign('https://wa.me/919990826002');
 }
 
 function Whatsapp() {
+    const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+    })
+
     return (
-        <div onClick={() => openWhatsapp()} className='whatsapp-redirect-container'>
+        <div>
+        {
+            isDesktopOrLaptop ? <div onClick={() => openWhatsapp()} className='whatsapp-redirect-container'>
             <img src={WhatsappIcon} alt="whatsapp" className="whatsapp-redirect" />
+            </div> 
+            :  <div onClick={() => openWhatsappMobile()} className='whatsapp-redirect-container'>
+            <img src={WhatsappIcon} alt="whatsapp" className="whatsapp-redirect" />
+            </div> 
+        }
         </div>
     )
 }
